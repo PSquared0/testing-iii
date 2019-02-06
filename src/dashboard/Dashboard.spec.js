552ab80ef9cde804914910c5e020894e754f0409
Pascale Pierre <pascaleee@gmail.com>
2019-02-05 19:52:23 -0600

    inital commit

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from 'react'
+describe('Dashboard', () => {
+  test('it renders the control and the display', () => {
+    // Arrange
+    const props = {
+      comment: 'React Testing Library is great',
+      author: 'Luke Ghenco'
+    }
+  })
+})
